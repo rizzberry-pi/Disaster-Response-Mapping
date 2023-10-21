e7d58c879cbb30bca626dd1ae50dd9fe5589aacc
energy-in-joles <nzmjoel@gmail.com>
2023-10-21 14:18:50 +0100

    test

--- main.py
+++ main.py
@@ -1,26 +1,2 @@
-from flask import Flask, render_template, url_for
-app = Flask(__name__)
-
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
-
-
-@app.route("/")
-def home():
-    return render_template('main.html', posts=posts)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+x = 5
+print(x)
